e82c4a7ae71860e2da92231d042c4b76f5a3e35e
Shivamteotia96 <chaudharyshivam82186@gmail.com>
2026-04-29 13:06:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll('a').forEach(a=>{
+  a.onclick = function(e){
+    if(this.hash){
+      e.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({behavior:'smooth'});
+    }
+  }
+});
